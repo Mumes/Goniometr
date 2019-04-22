14e69b69de7d2afb653a471704f5fd6b60d42abf
Mumes <rodionov.max88@gmail.com>
2019-04-22 16:44:06 +0300

    220419 work commit, just a few Messages added

--- GonFactory/UartCoordinateReaderMessages.cs
+++ GonFactory/UartCoordinateReaderMessages.cs
@@ -9,24 +9,43 @@ namespace GonCommand
     abstract class UartCoordinateReaderRequest : IUartMessage
     {
         protected ushort StartRequest = 0xAACC;
-        protected byte Command { get; set;}
-        public byte[] RequestBytes { get; private set;}
-        public void GetMesBytes()
+        protected byte Command { get; set; }
+        protected byte SecondCommand { get; set; }
+        protected byte BodyCommandFirst { get; set; }
+        protected byte BodyCommandSecond { get; set; }
+        public byte[] RequestBytes { get; private set; }
+        public virtual void GetMesBytes()
         {
-            
-        }
 
+        }
         public bool Validate()
         {
             return true;
         }
     }
+
+    class UartCoordinateReaderRequestMaschtab: UartCoordinateReaderRequest
+    {
+        public byte Axis { get; set; }
+        public float Val { get; set; }
+        public UartCoordinateReaderRequestMaschtab(byte _axis, float _val)
+        {
+            Command=Axis =_axis;
+            Val = _val;
+            Array.Copy(BitConverter.GetBytes(Val),RequestBytes,4,
+        }
+    }
+
+
+
+
+
     abstract class UartCoordinateReaderAnswer : IUartMessage
     {
 
         public void GetMesBytes()
         {
-
+            
         }
 
         public bool Validate()
